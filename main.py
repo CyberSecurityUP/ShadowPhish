b8951ee8311f03caf99c6936fb659b795ad7fd4b
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2025-04-18 10:30:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import sys
-import os
-import subprocess
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QTimer
-from frontend.splash_screen import ShadowPhishSplash
+from frontend.splash_lang import SplashInicial
 from frontend.lang_selector import LanguageSelector
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # Splash animado
-    splash = ShadowPhishSplash()
+    splash = SplashInicial()
     splash.show()
 
-    # Mostra o seletor após o splash
-    def show_lang_selector():
-        app.lang_window = LanguageSelector()  # Mantém viva a janela
-        app.lang_window.show()
-        splash.finish(app.lang_window)
+    # Adicione uma variável global para manter a janela viva
+    global lang_selector_window
+    lang_selector_window = None
 
-    QTimer.singleShot(2500, show_lang_selector)
+    def show_lang_selector():
+        global lang_selector_window
+        lang_selector_window = LanguageSelector(app)
+        lang_selector_window.show()
+        splash.finish(lang_selector_window)
+
+    QTimer.singleShot(2000, show_lang_selector)
+
     sys.exit(app.exec_())
